99e95587bd8e7815f42af90afec74433ff90d737
27mohammad <27mohammad.g@gmail.com>
2023-01-26 23:01:27 +0530

    first commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="position-fixed footer text-center bg-primary text-white bt-0">
+      &copy; copyrights belongs to Mr. new
+    </div>
+  );
+};
---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div className="text-center bg-primary text-white header">
+      This is my header.
+    </div>
+  );
+};
